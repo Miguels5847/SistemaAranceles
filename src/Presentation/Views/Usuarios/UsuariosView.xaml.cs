5723a9c19390554c03c9428c690c1aa08f7e79e7
CI Pipeline Runner <ci-runner@example.com>
2026-04-07 23:01:20 -0500

    Ruedita animada en loading de usuarios (rotación suave 1s)

--- src/Presentation/Views/Usuarios/UsuariosView.xaml.cs
+++ src/Presentation/Views/Usuarios/UsuariosView.xaml.cs
@@ -1,4 +1,7 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using SistemaAranceles.Presentation.ViewModels.Usuarios;
 
 namespace SistemaAranceles.Presentation.Views.Usuarios;
@@ -8,5 +11,20 @@ public partial class UsuariosView : UserControl
     public UsuariosView()
     {
         InitializeComponent();
+        this.Loaded += (s, e) => IniciarAnimacionCarga();
+    }
+
+    private void IniciarAnimacionCarga()
+    {
+        var rotation = new RotateTransform { CenterX = 24, CenterY = 24 };
+        var animation = new DoubleAnimation
+        {
+            From = 0,
+            To = 360,
+            Duration = TimeSpan.FromSeconds(1),
+            RepeatBehavior = RepeatBehavior.Forever
+        };
+        rotation.BeginAnimation(RotateTransform.AngleProperty, animation);
+        LoadingSymbol.RenderTransform = rotation;
     }
 }
